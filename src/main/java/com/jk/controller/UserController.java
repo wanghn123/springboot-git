439fedefa9023d874042bf51f6ba0980ef44ecf1
wanghn123 <3219925734@qq.com>
2020-08-19 15:04:58 +0800

    hello方法

--- src/main/java/com/jk/controller/UserController.java
+++ src/main/java/com/jk/controller/UserController.java
@@ -1,4 +1,12 @@
 package com.jk.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class UserController {
+
+    @RequestMapping("/hi")
+    public String hi (){
+        return "你好";
+    }
+
 }
